9b0d9762dd0e29397f22bc0b42747ac6d464b1e9
Zhaoyue Zhang <z5447138@ad.unse.edu.au>
2024-06-21 14:04:42 +1000

    [MODIFY] modify database.sql

--- database.sql
+++ database.sql
@@ -1,17 +1,26 @@
--- 创建t_user表
-CREATE TABLE t_user (
+DROP DATABASE IF EXISTS 'e_invoice_db';
+CREATE DATABASE IF NOT EXISTS 'e_invoice_db';
+USE 'e_invoice_db';
+
+SET NAMES utf8mb4;
+
+
+-- t_user table create
+DROP TABLE IF EXISTS 't_user';
+CREATE TABLE 't_user' (
                         user_id BIGINT PRIMARY KEY AUTO_INCREMENT,
                         email VARCHAR(255) NOT NULL,
                         firstname VARCHAR(255),
                         lastname VARCHAR(255),
-                        username VARCHAR(255) UNIQUE,
+                        username VARCHAR(255),
                         password VARCHAR(255) NOT NULL,
                         update_time DATETIME,
                         create_time DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
--- 创建t_file表
-CREATE TABLE t_file (
+-- t_file table create
+DROP TABLE IF EXISTS 't_file';
+CREATE TABLE 't_file' (
                         file_id INT PRIMARY KEY AUTO_INCREMENT,
                         file_name VARCHAR(255) NOT NULL,
                         user_id BIGINT NOT NULL,
@@ -20,8 +29,9 @@ CREATE TABLE t_file (
                         FOREIGN KEY (user_id) REFERENCES t_user(user_id)
 );
 
--- 创建t_login_fail表
-CREATE TABLE t_login_fail (
+-- t_login_fail create
+DROP TABLE IF EXISTS 't_login_fail';
+CREATE TABLE 't_login_fail' (
                               login_fail_id BIGINT PRIMARY KEY AUTO_INCREMENT,
                               user_id BIGINT,
                               user_type INT,
