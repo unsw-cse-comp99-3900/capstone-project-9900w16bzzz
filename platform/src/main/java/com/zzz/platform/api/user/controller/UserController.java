d5ec684b34a3fe3251d9de42445d67eb764c715d
Zhaoyue Zhang <z5447138@ad.unse.edu.au>
2024-06-23 22:42:22 +1000

    [MODIFY] Connect database, user CRUD

--- platform/src/main/java/com/zzz/platform/api/user/controller/UserController.java
+++ platform/src/main/java/com/zzz/platform/api/user/controller/UserController.java
@@ -1,14 +1,14 @@
 package com.zzz.platform.api.user.controller;
 
 import com.zzz.platform.api.user.domain.UserAddForm;
+import com.zzz.platform.api.user.domain.UserPwdUpdateForm;
+import com.zzz.platform.api.user.entity.UserEntity;
 import com.zzz.platform.api.user.service.UserService;
 import com.zzz.platform.common.annoation.NoNeedLogin;
 import com.zzz.platform.common.domain.ResponseDTO;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
@@ -36,7 +36,7 @@ public class UserController {
 
     /**
      * @author Zhaoyue Zhang
-     * chang user password
+     * 修改用户登录密码
      * @param userPwdUpdateForm
      * @return
      */
@@ -57,7 +57,7 @@ public class UserController {
     }
 
     /**
-     * @author Zhaooyue Zhang
+     * @author Zhaoyue Zhang
      * 根据userId查询用户信息
      * @param userId
      * @return
@@ -68,7 +68,7 @@ public class UserController {
     }
 
     /**
-     * 根据用户id删除用户
+     * 根据用户id删除用户  物理删除
      * @param userId
      * @return
      */
