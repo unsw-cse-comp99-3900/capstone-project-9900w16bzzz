61b68c3246dacb44e55de7d094b3eec7009405b5
JohnYan15 <8763163+johnyan15@user.noreply.gitee.com>
2024-08-03 18:42:06 +1000

    [MODIFY] modify the file path

--- frontend/src/Components/CreateInvoice/InvoiceCreationComponents/ChooseConvertOption.jsx
+++ frontend/src/Components/CreateInvoice/InvoiceCreationComponents/ChooseConvertOption.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-import { ReactComponent as ArrowIcon } from "../../images/arrow.svg";
+import { ReactComponent as ArrowIcon } from "../../../images/arrow.svg";
 import { usePopup } from "../../PopupWindow/PopupContext";
 
 /**
